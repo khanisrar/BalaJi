f680105f65c3102a4f56589ae4d24833daaa6693
khanisrar <khan.israr.lap.7651@gmail.com>
2023-05-03 08:16:47 +0530

    balaji

---
+++ js/footer.js
@@ -0,0 +1,92 @@
+document.write('<div class="footer-widgets">');
+document.write('  <div class="container"> ');
+document.write('    <div class="row"> ');
+document.write('      <div class="col-12 col-md-6 col-lg-3"> ');
+document.write('        <div class="foot-about"> ');
+document.write(
+  '          <h2><a class="foot-logo" href="#"><img src="images/main_logo.jpg" alt=""></a></h2>'
+);
+document.write(
+  "          <p>Manufacture & Supply of rubber Moulded, Rubber to metal bonded item, Extruded rubber hose and braided hose(LPG & Industrial Hose ) & Thermoplastic Polyurethane (TPU) & Railways Rubber to Metal Bonded Products</p>"
+);
+document.write('          <ul class="d-flex flex-wrap align-items-center">');
+document.write(
+  '            <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>'
+);
+document.write(
+  '            <li><a href="#"><i class="fa fa-facebook"></i></a></li>'
+);
+document.write(
+  '            <li><a href="#"><i class="fa fa-twitter"></i></a></li>'
+);
+document.write(
+  '            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>'
+);
+document.write(
+  '            <li><a href="#"><i class="fa fa-behance"></i></a></li>'
+);
+document.write(
+  '            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>'
+);
+document.write("          </ul>");
+document.write("        </div>");
+document.write("      </div>");
+document.write('      <div class="col-12 col-md-6 col-lg-3 mt-5 mt-md-0"> ');
+document.write("        <h2>We are Into</h2>");
+document.write("        <ul>");
+document.write("          <li>&#10022; Rubber Moulded </li>");
+document.write("          <li>&#10022; Rubber to metal bonded item</li>");
+document.write("          <li>&#10022; Extruded rubber hose </li>");
+document.write("          <li>&#10022; Extruded braided hose</li>");
+document.write("          <li>&#10022; Extruded Industrial Hose </li>");
+document.write("          <li>&#10022; Thermoplastic Polyurethane (TPU)</li>");
+document.write("          <li>&#10022; Railways Rubber to Metal Bonded</li>");
+document.write("        </ul>");
+document.write("      </div>");
+document.write('      <div class="col-12 col-md-6 col-lg-3 mt-5 mt-md-0"> ');
+document.write("        <h2>Top Menu</h2>");
+document.write("        <ul>");
+document.write('          <li><a href="index.html">&#x2714; Home</a></li>');
+document.write('          <li><a href="about.html">&#x2714; About Us</a></li>');
+
+document.write(
+  '          <li><a href="our-products.html">&#x2714; Products</a></li>'
+);
+document.write(
+  '          <li><a href="quality.html">&#x2714; Quality</a></li>'
+);
+document.write(
+  '          <li><a href="contact.html">&#x2714; Contact Us</a></li>'
+);
+document.write("        </ul>");
+document.write("      </div>");
+document.write('      <div class="col-12 col-md-6 col-lg-3 mt-5 mt-md-0"> ');
+document.write('        <div class="foot-contact"> ');
+document.write("          <h2>Contact Us</h2>");
+document.write("          <ul>");
+document.write(
+  '            <li><i class="fa fa-phone"></i><span><a href="tel:+91 8689972899">+91 8689972899</a> / <a href="tel:+91 9820805605">9820805605</a></span></li>'
+);
+document.write(
+  '            <li><i class="fa fa-envelope"></i><span><a href="mailto:balajirubberfactory@gmail.com">balajirubberfactory@gmail.com</a></span></li>'
+);
+document.write(
+  '            <li><i class="fa fa-map-marker"></i><span>54/31, D-II Block, Chinchwad, MIDC, Pune, Maharashtra - 411019</span></li>'
+);
+document.write("          </ul>");
+document.write("        </div>");
+document.write("      </div>");
+document.write("    </div>");
+document.write("  </div>");
+document.write("</div>");
+document.write('<div class="footer-bar">');
+document.write('     <div class="container">');
+document.write('        <div class="row">     ');
+document.write('      <div class="col-12"> ');
+document.write(
+  '        <p class="m-0">Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | Balaji Rubber Factory | Managed by <a href="http://bstarsw.com/" target="blank" style="color:#fff">BrilliantStar Software</a></p>'
+);
+document.write("      </div>");
+document.write("        </div>");
+document.write("      </div>");
+document.write("</div>");
---
+++ js/top-header.js
@@ -0,0 +1,21 @@
+document.write(
+  '<div class="row flex-wrap justify-content-center justify-content-lg-between align-items-lg-center">'
+);
+document.write(
+  '     <div class="col-12 col-lg-8 d-none d-md-flex flex-wrap justify-content-center justify-content-lg-start mb-3 mb-lg-0">'
+);
+document.write(
+  '           <div class="header-bar-email">Email: <a href="mailto:balajirubberfactory@gmail.com">balajirubberfactory@gmail.com</a></div>'
+);
+document.write(
+  '           <div class="header-bar-text"><p>Contact: <span><a href="tel:+91 8689972899" style="color:#fff">+91 8689972899</a> / <a href="tel:+91 9820805605" style="color:#fff">9820805605</a></span></p></div>'
+);
+document.write("     </div>");
+document.write(
+  '     <div class="col-12 col-lg-4 d-flex flex-wrap justify-content-center justify-content-lg-end align-items-center">'
+);
+document.write(
+  '           <div class="donate-btn"><a href="contact.html">Enquiry Now</a></div>'
+);
+document.write("     </div>");
+document.write("</div>");
